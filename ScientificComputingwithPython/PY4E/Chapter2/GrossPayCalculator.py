d0fb5c6300f5c68b9664282f6ab302fedd917a71
conradmauritz <79597329+conradmauritz@users.noreply.github.com>
2021-12-04 11:17:16 +0100

    Create GrossPayCalculator.py

---
+++ ScientificComputingwithPython/PY4E/Chapter2/GrossPayCalculator.py
@@ -0,0 +1,5 @@
+# Calculate the gross pay based on the hours and rate provided by the user
+
+hours = float(input("Enter the amount of hours worked: "))
+rate = float(input("Enter the hourly payment rate: "))
+print("The total payment amount is", (hours * rate))
