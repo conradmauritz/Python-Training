08c4d4344dbd99db13de4a55bc4f7ebabc41b403
conradmauritz <79597329+conradmauritz@users.noreply.github.com>
2021-12-04 12:30:01 +0100

    Create GrossPayCalculatorWithOvertimeExceptionHandling.py

---
+++ ScientificComputingwithPython/PY4E/Chapter3/GrossPayCalculatorWithOvertimeExceptionHandling.py
@@ -0,0 +1,19 @@
+# Calculate the gross pay based on the hours and rate provided by the user
+# If the hours provided are more than 40 then the payment rate for the hours over 40
+# will be 1.5 times more
+
+try:
+    hours_worked = float(input("Enter the amount of hours worked: "))
+    payment_rate = float(input("Enter the hourly payment rate: "))
+
+    standard_hours = 40
+
+    if hours_worked > standard_hours:
+        overtime_hours = hours_worked - standard_hours
+        gross_pay = round((standard_hours * payment_rate) + (overtime_hours * (payment_rate * 1.5)), 2)
+    else:
+        gross_pay = round((standard_hours * payment_rate), 2)
+
+    print("The total payment amount is:", gross_pay)
+except Exception as e:
+    print("Please enter only numerical values")
