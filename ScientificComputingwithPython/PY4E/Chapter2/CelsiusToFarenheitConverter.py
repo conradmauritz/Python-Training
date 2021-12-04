f1badb871619bb8424ae058c5ab80fb7718503c5
conradmauritz <79597329+conradmauritz@users.noreply.github.com>
2021-12-04 11:50:44 +0100

    Create CelsiusToFarenheitConverter.py

---
+++ ScientificComputingwithPython/PY4E/Chapter2/CelsiusToFarenheitConverter.py
@@ -0,0 +1,5 @@
+# Convert the provided Celsius temperature into Fahrenheit
+
+celsius_temp = float(input("Enter a temperature in Celsius: "))
+fahrenheit_temp = ((celsius_temp * (9 / 5)) + 32)
+print("The temperature in Fahrenheit is:", fahrenheit_temp)
